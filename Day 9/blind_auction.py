debb98eac15115a7a64dc9564605d1ef6d78dd44
Furkan Akram <furkan.akram@ontariotechu.net>
2026-08-16 15:53:59 -0700

    added the os function
    os function is used to cleanly clear out the terminal to prevent users from knowing who bid how much

--- Day 9/blind_auction.py
+++ Day 9/blind_auction.py
@@ -1,4 +1,5 @@
 from  ASCII_Art import Auction
+import os
 
 #print the ASCII art
 print (Auction[0])
@@ -11,12 +12,8 @@ print ("Hello, Welcome to the Blind Auction!")
 input ("Press Enter to continue...")
 
 def max_bid (bidder_list):
-    highest_bid = 0
-    highest_bidder = ""
-    for bidder in bidder_list:
-        if bidder_list[bidder] > highest_bid:
-            highest_bid = bidder_list[bidder]
-            highest_bidder = bidder
+    highest_bid = max(bidder_list.values())
+    highest_bidder = max(bidder_list, key=bidder_list.get)
 
     return (f"The highest bid is: ${highest_bid} by {highest_bidder}")
 
@@ -24,11 +21,13 @@ while True:
     #ask for the bidder's name and bid
     bidder = input("Whats your name? ")
     bidder_list[bidder] = int(input("Whats your bid? $"))
-    print ("\n")
 
+    #clear the screen for the next bidder
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
     #check to see if there are any other bidders, if not print the highest bid and break the loop
     if (input("Are there any other bidders? Type 'yes' or 'no': ").lower() == "no"):
-        print ("\n") 
         print (max_bid(bidder_list))
         break
+    
 
